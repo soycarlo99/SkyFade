b504e58bce2588a76e45e4702eaff2efadbe0ae3
carlodelos90 <carlodelos90@gmail.com>
2025-04-01 21:03:43 +0200

    WIP

--- src/SunsetMap.jsx
+++ src/SunsetMap.jsx
@@ -227,7 +227,7 @@ function SunsetMap() {
         </p>
       </div>
 
-      <MapContainer center={[20, 0]} zoom={2} className="map-container">
+      <MapContainer center={[0, 0]} zoom={2} className="map-container">
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
